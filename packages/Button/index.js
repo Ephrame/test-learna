928a1f5a6557be41776fea22eef84876680123dc
Amélie VIOLLET <amelieviollet@MacBookProdAmelie.local>
2017-07-17 22:21:34 +0200

    button component

--- packages/Button/index.js
+++ packages/Button/index.js
@@ -1,18 +1,32 @@
-import React, {PropTypes} from 'react'
+'use strict';
 
-const Button = (props) => {
-  const kindClass = props.kind ? `btn-${props.kind}` : ``
-  return <button type='button' className={`btn ${kindClass} ${props.responsive ? '' : 'fixedSize'}`} {...props} >{props.children}</button>
-}
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Button = function Button(props) {
+  var kindClass = props.kind ? 'btn-' + props.kind : '';
+  return _react2.default.createElement(
+    'button',
+    Object.assign({ type: 'button', className: 'btn ' + kindClass + ' ' + (props.responsive ? '' : 'fixedSize') }, props),
+    props.children
+  );
+};
 
 Button.defaultProps = {
   responsive: true
-}
+};
 
 Button.PropTypes = {
-  responsive: PropTypes.bool
-}
+  responsive: _react.PropTypes.bool
+};
 
-Button.displayName = 'Button'
-Button.__meta = require('./package.json')
-export default Button
+Button.displayName = 'Button';
+Button.__meta = require('./package.json');
+exports.default = Button;
